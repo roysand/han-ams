5b8870631969d1ca009c5d0ef427cb2e0449acb0
Roy Sand <roy@sanddata.no>
2020-11-11 07:33:33 +0100

    Added simple reader - Python

---
+++ hantest01/hantest01.py
@@ -0,0 +1,22 @@
+import serial
+import codecs
+import sys
+
+ser = serial.Serial(
+    port='/dev/ttyUSB0',
+    baudrate=2400,
+    parity=serial.PARITY_NONE,
+    stopbits=serial.STOPBITS_ONE,
+    bytesize=serial.EIGHTBITS,
+    timeout=4)
+print("Connected to: " + ser.portstr)
+
+while True:
+    bytes = ser.read(1024)
+    if bytes:
+        print('Got %d bytes:' % len(bytes))
+        bytes = ('%02x' % int(codecs.encode(bytes, 'hex'), 16)).upper()
+        bytes = ' '.join(bytes[i:i+2] for i in range(0, len(bytes), 2))
+        print(bytes)
+    else:
+        print('Got nothing')
