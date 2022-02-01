ca430d8c6999bb06d0742a60afef744b0c011644
Roy Sand <roy@sanddata.no>
2022-02-01 15:53:17 +0100

    Added real data to daily function in stat

--- csharp/iot/src/Application/Common/Models/HourTotalVm.cs
+++ csharp/iot/src/Application/Common/Models/HourTotalVm.cs
@@ -1,21 +1,17 @@
 ﻿using System;
-using System.Collections;
-using System.Collections.Generic;
 
 namespace Application.Common.Models
 {
-    public class DailyTotalVm
+    public class HourTotalVm
     {
         public DateTime Date { get; set; }
         public decimal Value { get; set; }
         public string Description { get; set; }
         public string Unit { get; set; }
 
-        public IList<HourTotalVm> HoursTotal { get; set; }
-
-        public DailyTotalVm()
+        public HourTotalVm()
         {
-            HoursTotal = new List<HourTotalVm>();
+            
         }
     }
 }
--- csharp/iot/src/Infrastructure/Repositories/StatRepository.cs
+++ csharp/iot/src/Infrastructure/Repositories/StatRepository.cs
@@ -33,17 +33,30 @@ namespace Infrastructure.Repositories
 
         public async Task<DailyTotalVm> DailyTotal(DateTime date, CancellationToken cancellationToken)
         {
-            var data = await (from detail in _context.DetailSet
-                    where detail.TimeStamp.Date == date.Date && detail.ValueNum != -1
-                    select detail.ValueNum)
-                .AverageAsync(cancellationToken);
+            // var data = await (from detail in _context.DetailSet
+            //         where detail.TimeStamp.Date == date.Date && detail.ValueNum != -1
+            //         select detail.ValueNum)
+            //     .AverageAsync(cancellationToken);
+
+            var powerByHourByDay = await (from detail in _context.DetailSet
+                where detail.ObisCode == "1-0:1.7.0.255" && detail.TimeStamp.Date == DateTime.Now.Date
+                group detail by new { date = detail.TimeStamp.Date, hour = detail.TimeStamp.Hour }
+                into g
+                select new HourTotalVm()
+                {
+                    Date = new DateTime(g.Key.date.Year, g.Key.date.Month, g.Key.date.Day,g.Key.hour,0,0),
+                    Value = g.Average(x => x.ValueNum),
+                    Description = "Hour Total",
+                    Unit = "kW"
+                }).ToListAsync(cancellationToken);
             
             return new DailyTotalVm()
             {
                 Date = date,
-                Value = data,
-                Description = "Daily average",
-                Unit = "kW"
+                Value = powerByHourByDay.Sum(s => s.Value),
+                Description = "Daily sum",
+                Unit = "kW",
+                HoursTotal = powerByHourByDay
             };
         }
     }
