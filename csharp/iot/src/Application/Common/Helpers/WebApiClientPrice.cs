3b47e333f32cb10554e658649f730d61a3eb9c3a
Roy Sand <roy@sanddata.no>
2022-05-22 17:14:20 +0200

    1. commit in this branch

---
+++ csharp/iot/src/Application/Common/Helpers/WebApiClientPrice.cs
@@ -0,0 +1,18 @@
+﻿using System.Net.Http;
+using Application.Common.Interfaces;
+using Microsoft.Extensions.Configuration;
+
+namespace Application.Common.Helpers
+{
+    public class WebApiClientPrice : HttpClient, IWebApiClientPrice
+    {
+        private readonly IConfiguration _configuration;
+
+        public WebApiClientPrice(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+        
+        
+    }
+}
