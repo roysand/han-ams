336def398cefac296dabeb9bf8f6afae86feb701
Roy Sand <roy@sanddata.no>
2022-07-19 05:57:33 +0200

    1. commit for test stat repo

---
+++ csharp/iot/tests/TestStatRepository/Program.cs
@@ -0,0 +1,44 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using Application.Common.Interfaces;
+using Domain.Entities;
+using Infrastructure;
+using Infrastructure.Clients;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+var configuration = new ConfigurationBuilder()
+    .AddJsonFile("local.settings.json", optional:true, reloadOnChange: true)
+    .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+    .AddEnvironmentVariables()
+    .Build();
+
+var builder = CreateHostBuilder(args, configuration);
+builder.ConfigureAppConfiguration(app => app.AddConfiguration(configuration));
+var app = builder.Build();
+
+Console.WriteLine("Hello, World!");
+var statRepository = app.Services.GetService<IStatRepository<Domain.Entities.Detail>>();
+if (statRepository != null)
+{
+    var result = await statRepository.DailyTotal(DateTime.Now, new CancellationToken());
+}
+
+return; 
+
+static IHostBuilder CreateHostBuilder(string[] args, IConfiguration configuration)
+{
+    var hostBuilder = Host.CreateDefaultBuilder(args)
+        .ConfigureAppConfiguration((context, builder) =>
+        {
+            builder.SetBasePath(Directory.GetCurrentDirectory());
+        })
+        .ConfigureServices((context, services) =>
+        {
+            // services.AddSingleton<ITemplatePostCodesRepository<TemplatePostCodes>, TemplatePostCodesRepository<TemplatePostCodes>>();
+            services.AddInfrastructure(configuration);
+        });
+
+    return hostBuilder;
+}
