c8a029fdb459f5113a0c4ec131be89da914910f8
Roy Sand <roy@sanddata.no>
2022-09-28 21:22:14 +0200

    local changes on iot-ha43

---
+++ csharp/iot/scripts/getnewprices-1.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+cd /home/roy/repo/han-ams/csharp/iot/tests/TestPriceClient/
+/snap/bin/dotnet run
+
+
--- csharp/iot/scripts/getnewprices.sh
+++ csharp/iot/scripts/getnewprices.sh
@@ -1,9 +1,9 @@
-#! /bin/sh
-tmux new-session -d -s iot-price
+#!/bin/sh
+/usr/bin/tmux new-session -d -s iot-price
 # tmux detach -s iot-price ENTER
 sleep 1
-tmux send-keys -t iot-price 'ls -la' ENTER
-tmux send-keys -t iot-price 'cd /home/roy/repo/han-ams/csharp/iot/tests/TestPriceClient/' ENTER
-tmux send-keys -t iot-price 'dotnet run  &>> ~/log/price.log; tmux wait -S iot' ENTER
-tmux wait iot
-tmux kill-session -t iot-price
+/usr/bin/tmux send-keys -t iot-price 'ls -la' ENTER
+/usr/bin/tmux send-keys -t iot-price 'cd /home/roy/repo/han-ams/csharp/iot/tests/TestPriceClient/' ENTER
+/usr/bin/tmux send-keys -t iot-price '/snap/bin/dotnet run; tmux wait -S iot' ENTER
+/usr/bin/tmux wait iot
+/usr/bin/tmux kill-session -t iot-price
