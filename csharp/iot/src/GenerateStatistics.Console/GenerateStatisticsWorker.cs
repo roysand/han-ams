e62cc160e6315bc9bd88e44af25e05a855cda124
Roy Sand <roy@sanddata.no>
2023-01-24 19:17:52 +0100

    1. commit in this branch

---
+++ csharp/iot/src/GenerateStatistics.Console/GenerateStatisticsWorker.cs
@@ -0,0 +1,38 @@
+﻿namespace GenerateStatistics.Console;
+
+public class GenerateStatisticsWorker : BackgroundService
+{
+    private readonly ILogger<GenerateStatisticsWorker> _logger;
+    private readonly TimeSpan _periode = TimeSpan.FromSeconds(5);
+    public bool IsEnabled { get; set; }
+    
+    public GenerateStatisticsWorker(ILogger<GenerateStatisticsWorker> logger)
+    {
+        _logger = logger;
+    }
+    
+    protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+    {
+        _logger.LogInformation("Hello from worker");
+        using PeriodicTimer timer = new PeriodicTimer(_periode);
+
+        while (!stoppingToken.IsCancellationRequested && await timer.WaitForNextTickAsync(stoppingToken))
+        {
+            try
+            {
+                if (IsEnabled)
+                {
+                    _logger.LogInformation("Worker is enabled!");
+                }
+                else
+                {
+                    _logger.LogInformation("Worker is NOT enabled!");
+                }
+            }
+            catch (Exception e)
+            {
+                _logger.LogError($"Failed to execute GenerateStatisticWorker - exception message: '{e.Message}'");
+            }
+        }
+    }
+}
---
+++ csharp/iot/src/GenerateStatistics.Console/Program.cs
@@ -0,0 +1,26 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using GenerateStatistics.Console;
+using Infrastructure;
+using Infrastructure.Config;
+
+Console.WriteLine("Generate Statistics Console app starting ..");
+
+var builder = WebApplication.CreateBuilder(args);
+
+var configuration = builder.Configuration
+    .AddEnvironmentVariables()
+    .AddJsonFile("local.settings.json", optional: false, reloadOnChange: true)
+    .AddJsonFile("appsettings.Development.json", optional: false, reloadOnChange: true)
+    .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+    .AddUserSecrets<Program>();
+
+var config = new Config(builder.Configuration);
+
+// Add services to the container.
+builder.Services.AddInfrastructure();
+builder.Services.AddHostedService<GenerateStatisticsWorker>();
+
+var app = builder.Build();
+
+app.Run();
