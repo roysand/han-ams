d5be3c954d1bcc7a1e7e783202935b4da255a6b0
Roy Sand <roy@sanddata.no>
2023-01-24 22:23:10 +0100

    Added api to control worker

---
+++ csharp/iot/src/GenerateStatistics.Console/PeriodicHostedServiceState.cs
@@ -0,0 +1,3 @@
+﻿namespace GenerateStatistics.Console;
+
+record PeriodicHostedServiceState(bool IsEnabled);
--- csharp/iot/src/GenerateStatistics.Console/Program.cs
+++ csharp/iot/src/GenerateStatistics.Console/Program.cs
@@ -19,8 +19,29 @@ var config = new Config(builder.Configuration);
 
 // Add services to the container.
 builder.Services.AddInfrastructure();
-builder.Services.AddHostedService<GenerateStatisticsWorker>();
+
+// Register as singleton first so it can be injected through Dependency Injection
+builder.Services.AddSingleton<GenerateStatisticsWorker>();
+
+builder.Services.AddHostedService(
+    provider => provider.GetRequiredService<GenerateStatisticsWorker>());
+
 
 var app = builder.Build();
+
+
+app.MapGet("/", () => "Hello World!");
+app.MapGet("/background", (
+    GenerateStatisticsWorker service) =>
+{
+    return new PeriodicHostedServiceState(service.IsEnabled);
+});
+
+app.MapMethods("/background", new[] { "PATCH" }, (
+    PeriodicHostedServiceState state, 
+    GenerateStatisticsWorker service) =>
+{
+    service.IsEnabled = state.IsEnabled;
+});
 
 app.Run();
