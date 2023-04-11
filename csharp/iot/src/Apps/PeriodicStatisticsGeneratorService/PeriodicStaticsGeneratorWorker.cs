c84b50f280157301c1a57cb79841b33b4787f39c
Roy Sand <roy@sanddata.no>
2023-04-11 20:36:41 +0200

    Added logging line back again

--- csharp/iot/src/Apps/PeriodicStatisticsGeneratorService/PeriodicStaticsGeneratorWorker.cs
+++ csharp/iot/src/Apps/PeriodicStatisticsGeneratorService/PeriodicStaticsGeneratorWorker.cs
@@ -41,7 +41,9 @@ public class PeriodicStaticsGeneratorWorker : BackgroundService
                 if (IsEnabled)
                 {
                     now = _dateTime.Now;
-
+                    
+                    _logger.LogInformation("{Now}- Timer is running!?", now);
+                    
                     if (now.Minute - _lastRunTimeMinute.Minute >= 1 && now.Second >= 2)
                     {
                         _logger.LogInformation("Creating statistics for minute");
