4ee21ac259d05073cce3c34c6dcb72195735d78d
Roy Sand <roy@sanddata.no>
2023-04-17 15:06:45 +0200

    Change runtime for hour generating statistics to 5 past every hour

--- csharp/iot/src/Apps/PeriodicStatisticsGeneratorService/PeriodicStaticsGeneratorWorker.cs
+++ csharp/iot/src/Apps/PeriodicStatisticsGeneratorService/PeriodicStaticsGeneratorWorker.cs
@@ -52,7 +52,7 @@ public class PeriodicStaticsGeneratorWorker : BackgroundService
                         await _statRepository.GenerateMinutePowerUsageStatistics(stoppingToken);
                     }
 
-                    if (Math.Abs((now.Hour - _lastRunTimeHour.Hour)) >= 1 && now.Minute >= 1)
+                    if (Math.Abs((now.Hour - _lastRunTimeHour.Hour)) >= 1 && now.Minute >= 5)
                     {
                         _logger.LogInformation("Creating statistics for hour");
                         _lastRunTimeHour = now;
