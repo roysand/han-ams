2c2ee9dda2de938312e694a08f470f17405e2895
Roy Sand <roy@sanddata.no>
2023-04-17 15:14:36 +0200

    Change runtime for hour generating statistics to 2 min past every hour

--- csharp/iot/src/Apps/PeriodicStatisticsGeneratorService/PeriodicStaticsGeneratorWorker.cs
+++ csharp/iot/src/Apps/PeriodicStatisticsGeneratorService/PeriodicStaticsGeneratorWorker.cs
@@ -52,7 +52,7 @@ public class PeriodicStaticsGeneratorWorker : BackgroundService
                         await _statRepository.GenerateMinutePowerUsageStatistics(stoppingToken);
                     }
 
-                    if (Math.Abs((now.Hour - _lastRunTimeHour.Hour)) >= 1 && now.Minute >= 5)
+                    if (Math.Abs((now.Hour - _lastRunTimeHour.Hour)) >= 1 && now.Minute >= 2)
                     {
                         _logger.LogInformation("Creating statistics for hour");
                         _lastRunTimeHour = now;
