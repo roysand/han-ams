4ff5fb3216216311b9a6d3da660aa4aaf2d8bc9c
Roy Sand <roy@sanddata.no>
2024-11-25 11:52:46 +0100

    modified and renamed reboot.sh to reboot-iot.sh

---
+++ csharp/iot/scripts/reboot-iot.sh
@@ -0,0 +1,22 @@
+﻿#!/bin/bash
+
+# Name of the tmux session
+SESSION_NAME="iot"
+
+# Directory containing the start.csproj file
+TARGET_DIR="/home/roy/repo/han-ams/csharp/iot/src/Apps/MqttReader.Console"
+
+# Check if the session already exists
+tmux has-session -t $SESSION_NAME 2>/dev/null
+
+# If the session does not exist, create it and start the .NET project
+if [ $? != 0 ]; then
+    # Create a new session and start it detached
+    tmux new-session -d -s $SESSION_NAME
+
+    # Send the command to change directory and run the project
+    tmux send-keys -t $SESSION_NAME "cd $TARGET_DIR; dotnet run MqttReader.Console -c Release" C-m
+fi
+
+# Attach to the session
+# tmux attach -t $SESSION_NAME
--- csharp/iot/scripts/reboot.sh
+++
@@ -1,3 +0,0 @@
-﻿#!/bin/sh
-cd /home/roy/repo/han-ams/csharp/iot/src/Apps/MqttReader.Console
-//home/roy/.dotnet/dotnet run MqttReader.Console -c Release
